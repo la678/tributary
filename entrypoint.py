991db03f00ab110a1453e326cdedca30a00b2c30
la678 <lanchuri@ttu.edu>
2024-07-09 21:39:41 -0500

    added bind to 0.0.0.0 to gunicorn and it is working

--- entrypoint.py
+++ entrypoint.py
@@ -1,7 +1,7 @@
 # import the flask web framework
 import json
 import redis as redis
-from flask import Flask, request
+from flask import Flask, request,jsonify
 from loguru import logger
 
 HISTORY_LENGTH = 10
@@ -15,25 +15,6 @@ app = Flask(__name__)
 @app.route('/record', methods=['POST'])
 def record_engine_temperature():
     # every time the /record endpoint is called, the code in this block is executed
-    # payload = request.get_json(force=True)
-    # logger.info(f"(*) record request --- {json.dumps(payload)} (*)")
-    #
-    # engine_temperature = payload.get("engine_temperature")
-    # logger.info(f"engine temperature to record is: {engine_temperature}")
-    #
-    # database = redis.Redis(host="redis", port=6379, db=0, decode_responses=True)
-    # database.lpush(DATA_KEY, engine_temperature)
-    # logger.info(f"stashed engine temperature in redis: {engine_temperature}")
-    #
-    # while database.llen(DATA_KEY) > HISTORY_LENGTH:
-    #     database.rpop(DATA_KEY)
-    # engine_temperature_values = database.lrange(DATA_KEY, 0, -1)
-    # logger.info(f"engine temperature list now contains these values: {engine_temperature_values}")
-    #
-    # logger.info(f"record request successful")
-    #
-    # # return a json payload, and a 200 status code to the client
-    # return {"success": True}, 200
 
     try:
         payload = request.get_json(force=True)
@@ -66,6 +47,6 @@ def record_engine_temperature():
         return jsonify({"error": "Internal server error"}), 500
 
 # practically identical to the above
-@app.route('/collect', methods=['POST'])
+@app.route('/collect', methods=['GET'])
 def collect_engine_temperature():
     return {"success": True}, 200
